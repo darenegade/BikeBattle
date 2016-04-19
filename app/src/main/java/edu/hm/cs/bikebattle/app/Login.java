fee202e27e936a3b7e51344253d41d8e15ee9040
munichsven <sschulz@hm.edu>
2016-04-19 14:43:55 +0200

    Login Funktioniert noch ohne Doku

--- app/src/main/java/edu/hm/cs/bikebattle/app/Login.java
+++ app/src/main/java/edu/hm/cs/bikebattle/app/Login.java
@@ -1,5 +1,6 @@
 package edu.hm.cs.bikebattle.app;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -11,27 +12,29 @@ import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.Scopes;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.OptionalPendingResult;
+import com.google.android.gms.common.api.Scope;
 
 public class Login extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener{
 
     private static final String TAG = "SignInBikeBattle";
     private static final int RC_SIGN_IN = 9001;
     private GoogleApiClient mGoogleApiClient;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         initCompoents();
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Signing in .....");
     }
 
     private void initCompoents(){
-        //ImageView viewLogo = (ImageView) findViewById(R.id.logo);
-        //viewLogo.setImageResource(R.drawable.fahrrad_comic_2);
-
-
       GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestEmail()
                 .build();
@@ -42,6 +45,7 @@ public class Login extends AppCompatActivity implements GoogleApiClient.OnConnec
                 .build();
 
         SignInButton login = (SignInButton) findViewById(R.id.sign_in_button);
+        login.setScopes(gso.getScopeArray());
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -53,6 +57,7 @@ public class Login extends AppCompatActivity implements GoogleApiClient.OnConnec
     private void signIn() {
         Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
         startActivityForResult(signInIntent, RC_SIGN_IN);
+        progressDialog.show();
     }
 
     @Override
@@ -62,24 +67,35 @@ public class Login extends AppCompatActivity implements GoogleApiClient.OnConnec
 
     @Override public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        String userName;
-        String userMail;
-        String userToken;
 
         if (requestCode == RC_SIGN_IN) {
             GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
-            if (result.isSuccess()) {
-                Log.d(TAG, "Login successful!!!");
-                GoogleSignInAccount acct = result.getSignInAccount();
-                userName = acct.getDisplayName();
-                userMail = acct.getEmail();
-                userToken = acct.getIdToken();
+            handleSignInResult(result);
+        }
+    }
 
-                Toast toast = Toast.makeText(getApplicationContext(), userMail, Toast.LENGTH_LONG);
-                toast.show();
-            } else {
-                Log.d(TAG, "Fehler beim Login: " + result.getStatus().toString());
-            }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    private void handleSignInResult(GoogleSignInResult result) {
+
+        String userName;
+        String userMail;
+        String userToken;
+        if (result.isSuccess()) {
+            Log.d(TAG, "Login successful!!!");
+            GoogleSignInAccount acct = result.getSignInAccount();
+            userName = acct.getDisplayName();
+            userMail = acct.getEmail();
+            userToken = acct.getIdToken();
+            
+            Toast toast = Toast.makeText(getApplicationContext(), "Erfolgreich angemeldet", Toast.LENGTH_LONG);
+            toast.show();
+        } else {
+            Log.d(TAG, "Fehler beim Login: " + result.getStatus().toString());
         }
     }
 }
