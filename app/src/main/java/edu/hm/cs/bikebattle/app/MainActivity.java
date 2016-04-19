bb959288fd507080cd7dbeeec23b734b5d2bddb3
Nils Bernhardt <nils.bernhardt@live.de>
2016-04-19 12:20:32 +0200

    Restored main activity.

--- app/src/main/java/edu/hm/cs/bikebattle/app/MainActivity.java
+++ app/src/main/java/edu/hm/cs/bikebattle/app/MainActivity.java
@@ -1,14 +1,9 @@
 package edu.hm.cs.bikebattle.app;
 
 import android.app.Activity;
-import android.location.Location;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-
-import edu.hm.cs.bikebattle.app.tracker.GoogleApiLocationTracker;
-import edu.hm.cs.bikebattle.app.tracker.LocationTracker;
 
 public class MainActivity extends Activity {
 
@@ -16,41 +11,6 @@ public class MainActivity extends Activity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-
-
-    final LocationTracker tracker;
-    tracker = new GoogleApiLocationTracker(this, 0);
-   // tracker = new AndroidLocationTracker(0, this);
-    new Thread() {
-      @Override
-      public void run() {
-
-        synchronized (tracker) {
-          while (!tracker.isReady())
-            try {
-              tracker.wait();
-            } catch (InterruptedException e) {
-              e.printStackTrace();
-            }
-        }
-        tracker.continueTracking();
-        try {
-
-
-          synchronized (this) {
-            wait(60000*3);
-          }
-          tracker.stop();
-          tracker.shutdown();
-          Log.d("Track", tracker.getTrack().size()+"");
-          for(Location l: tracker.getTrack())
-            Log.d("Location", l.toString());
-        } catch (InterruptedException e) {
-          e.printStackTrace();
-        }
-
-      }
-    }.start();
   }
 
 
