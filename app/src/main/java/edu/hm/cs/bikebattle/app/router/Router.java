9111bd88ea2a01ba19ccd5141ff94bb9746e83bb
Nils Bernhardt <nils.bernhardt@live.de>
2016-04-23 15:53:03 +0200

    Java doc.

--- app/src/main/java/edu/hm/cs/bikebattle/app/router/Router.java
+++ app/src/main/java/edu/hm/cs/bikebattle/app/router/Router.java
@@ -8,19 +8,57 @@ import edu.hm.cs.bikebattle.app.tracker.LocationTracker;
 
 /**
  * Created by Nils on 19.04.2016.
+ * <p/>
+ * Interface for a router. The router tracks the ride and navigates over a given route.
+ *
+ * @author Nils Bernhardt
+ * @version 1.0
  */
-public interface Router extends LocationTracker{
+public interface Router extends LocationTracker {
+  /**
+   * Checks if the user is currently in the start area.
+   *
+   * @return true if in start area.
+   */
   boolean isInStartArea();
 
+  /**
+   * Returns the route.
+   *
+   * @return route
+   */
   Route getRoute();
 
+  /**
+   * Returns the next target or null if the route is finished.
+   *
+   * @return the next target
+   */
   Location getNextTarget();
 
+  /**
+   * Returns true if all targets are reached.
+   *
+   * @return true if finished
+   */
   boolean isFinished();
 
+  /**
+   * Stops routing. Tracking is continued.
+   */
   void stopRouting();
 
+  /**
+   * Starts tracking, but only if the user is within the start area.
+   *
+   * @return true if tracking is started
+   */
   boolean startTracking();
 
+  /**
+   * Returns the result track.
+   *
+   * @return result
+   */
   Track getResult();
 }
