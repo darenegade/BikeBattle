32e8487d873c28cd81d27e7705a82aeb5429f761
Nils Bernhardt <nils.bernhardt@live.de>
2016-04-26 15:46:57 +0200

    java doc.

--- app/src/main/java/edu/hm/cs/bikebattle/app/data/DataConnector.java
+++ app/src/main/java/edu/hm/cs/bikebattle/app/data/DataConnector.java
@@ -8,7 +8,7 @@ import edu.hm.cs.bikebattle.app.modell.User;
 
 /**
  * Created by Nils on 26.04.2016.
- *
+ * <p/>
  * Interface for the connection to the data local and on the server.
  *
  * @author Nils Bernhardt
@@ -17,6 +17,7 @@ import edu.hm.cs.bikebattle.app.modell.User;
 public interface DataConnector {
   /**
    * Returns all Route which are within the distance to the given location.
+   *
    * @param location Location
    * @param distance maximum distance
    * @return all Routes
@@ -25,13 +26,15 @@ public interface DataConnector {
 
   /**
    * Returns the user with the given id.
+   *
    * @param id user id
    * @return user
    */
-  User getUserByID(int id);
+  User getUserById(int id);
 
   /**
    * Returns the user with the given name.
+   *
    * @param name user name
    * @return user
    */
@@ -39,6 +42,7 @@ public interface DataConnector {
 
   /**
    * Returns all tracks of the given user.
+   *
    * @param user User
    * @return tracks of the user
    */
@@ -46,6 +50,7 @@ public interface DataConnector {
 
   /**
    * Returns all routes of the given user.
+   *
    * @param user User
    * @return routes of the user
    */
@@ -53,40 +58,46 @@ public interface DataConnector {
 
   /**
    * Adds a track to the users database.
+   *
    * @param track new Track
-   * @param user owner
+   * @param user  owner
    */
   void addTrack(Track track, User user);
 
   /**
    * Deletes a track of the user.
+   *
    * @param track to delete
-   * @param user owner
+   * @param user  owner
    */
   void deleteTrack(Track track, User user);
 
   /**
    * Adds a route to the users database.
+   *
    * @param route new Route
-   * @param user owner
+   * @param user  owner
    */
   void addRoute(Route route, User user);
 
   /**
    * Deletes a route of the user.
+   *
    * @param route to delete
-   * @param user owner
+   * @param user  owner
    */
   void deleteRoute(Route route, User user);
 
   /**
    * Creates a new user.
+   *
    * @param user user
    */
   void createUser(User user);
 
   /**
    * Updates the user data.
+   *
    * @param user user
    */
   void changeUserData(User user);
