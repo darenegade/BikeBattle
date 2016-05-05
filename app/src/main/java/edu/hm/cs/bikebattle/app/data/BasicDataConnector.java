cff3a7d8331ed0bffbe601ea1817ff909af6cdc6
Nils Bernhardt <nils.bernhardt@live.de>
2016-05-05 18:09:17 +0200

    First implements

--- app/src/main/java/edu/hm/cs/bikebattle/app/data/BasicDataConnector.java
+++ app/src/main/java/edu/hm/cs/bikebattle/app/data/BasicDataConnector.java
@@ -5,9 +5,13 @@ import android.location.Location;
 import org.springframework.hateoas.Resource;
 
 import java.io.IOException;
+import java.util.Collection;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.UUID;
 
+import edu.hm.cs.bikebattle.app.api.domain.DriveDto;
+import edu.hm.cs.bikebattle.app.api.domain.RouteDto;
 import edu.hm.cs.bikebattle.app.api.domain.UserDto;
 import edu.hm.cs.bikebattle.app.api.rest.ClientFactory;
 import edu.hm.cs.bikebattle.app.api.rest.DriveClient;
@@ -16,6 +20,8 @@ import edu.hm.cs.bikebattle.app.api.rest.UserClient;
 import edu.hm.cs.bikebattle.app.modell.Route;
 import edu.hm.cs.bikebattle.app.modell.Track;
 import edu.hm.cs.bikebattle.app.modell.User;
+import edu.hm.cs.bikebattle.app.modell.assembler.RouteAssembler;
+import edu.hm.cs.bikebattle.app.modell.assembler.TrackAssembler;
 import edu.hm.cs.bikebattle.app.modell.assembler.UserAssembler;
 import retrofit2.Call;
 import retrofit2.Response;
@@ -39,6 +45,7 @@ public class BasicDataConnector implements DataConnector {
 
   @Override
   public Route[] getRoutesByLocation(Location location, float distance) {
+    //TODO api missing
     return new Route[0];
   }
 
@@ -46,9 +53,10 @@ public class BasicDataConnector implements DataConnector {
   public User getUserById(UUID id) {
     Call<Resource<UserDto>> call = userClient.findeOne(id);
     try {
+      //TODO check response code (see test cases) and handle errors
       Response<Resource<UserDto>> userDtoResponse = call.execute();
       Resource<UserDto> resource = userDtoResponse.body();
-      UserDto userDto = resource.getContent();//check response code (see test cases)
+      UserDto userDto = resource.getContent();
       User user = UserAssembler.toBean(userDto);
       return user;
     } catch (IOException e) {
@@ -59,17 +67,45 @@ public class BasicDataConnector implements DataConnector {
 
   @Override
   public List<User> getUserByName(String name) {
-    return null;
+    List<User>  users = new LinkedList<User>();
+    try {
+      //TODO check response code (see test cases) and handle errors
+      Collection<Resource<UserDto>> userDTOs = userClient.findByNameContainingIgnoreCase(name).execute().body().getContent();
+      for(Resource<UserDto> userDtoResource: userDTOs){
+       users.add( UserAssembler.toBean(userDtoResource.getContent()));
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    return users;
   }
 
   @Override
   public List<Track> getTracksByUser(User user) {
-    return null;
+    List<Track> tracks = new LinkedList<Track>();
+    try {
+      Collection<Resource<DriveDto>> driveDTOs = driveClient.findByOwnerOid(user.getOid()).execute().body().getContent();
+      for(Resource<DriveDto> driveDtoResource: driveDTOs){
+        tracks.add( TrackAssembler.toBean(driveDtoResource.getContent()));
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    return tracks;
   }
 
   @Override
   public List<Route> getRoutesByUser(User user) {
-    return null;
+    List<Route> routes = new LinkedList<Route>();
+    try {
+      Collection<Resource<RouteDto>> routeDTOs = routeClient.findByOwnerOid(user.getOid()).execute().body().getContent();
+      for(Resource<RouteDto> routeDtoResource: routeDTOs){
+        routes.add( RouteAssembler.toBean(routeDtoResource.getContent()));
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    return routes;
   }
 
   @Override
