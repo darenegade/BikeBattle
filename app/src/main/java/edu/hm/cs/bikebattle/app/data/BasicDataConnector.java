7e62aff25c0e2e705cb596645bfd81bf0cfdcf8f
Nils Bernhardt <nils.bernhardt@live.de>
2016-05-08 12:59:21 +0200

    java doc

--- app/src/main/java/edu/hm/cs/bikebattle/app/data/BasicDataConnector.java
+++ app/src/main/java/edu/hm/cs/bikebattle/app/data/BasicDataConnector.java
@@ -31,18 +31,27 @@ import retrofit2.Response;
  * Basic implementation for a connection to the backend.
  * No error handling!
  *
- * @version 1.0
  * @author Nils Bernhardt
+ * @version 1.0
  */
 public class BasicDataConnector implements DataConnector {
-
+  /**
+   * Backend user client.
+   */
   private final UserClient userClient;
-
+  /**
+   * Backend route client.
+   */
   private final RouteClient routeClient;
-
+  /**
+   * Backend drive/track client.
+   */
   private final DriveClient driveClient;
 
-  public BasicDataConnector(){
+  /**
+   * Creates the clients for the backend.
+   */
+  public BasicDataConnector() {
     userClient = ClientFactory.getUserClient();
     routeClient = ClientFactory.getRouteClient();
     driveClient = ClientFactory.getDriveClient();
@@ -64,23 +73,24 @@ public class BasicDataConnector implements DataConnector {
       UserDto userDto = resource.getContent();
       User user = UserAssembler.toBean(userDto);
       return user;
-    } catch (IOException e) {
-      e.printStackTrace();
+    } catch (IOException exception) {
+      exception.printStackTrace();
     }
     return null;
   }
 
   @Override
   public List<User> getUserByName(String name) {
-    List<User>  users = new LinkedList<User>();
+    List<User> users = new LinkedList<User>();
     try {
       //TODO check response code (see test cases) and handle errors
-      Collection<Resource<UserDto>> userDTOs = userClient.findByNameContainingIgnoreCase(name).execute().body().getContent();
-      for(Resource<UserDto> userDtoResource: userDTOs){
-       users.add( UserAssembler.toBean(userDtoResource.getContent()));
+      Collection<Resource<UserDto>> userDtos = userClient.findByNameContainingIgnoreCase(name)
+          .execute().body().getContent();
+      for (Resource<UserDto> userDtoResource : userDtos) {
+        users.add(UserAssembler.toBean(userDtoResource.getContent()));
       }
-    } catch (IOException e) {
-      e.printStackTrace();
+    } catch (IOException exception) {
+      exception.printStackTrace();
     }
     return users;
   }
@@ -89,12 +99,13 @@ public class BasicDataConnector implements DataConnector {
   public List<Track> getTracksByUser(User user) {
     List<Track> tracks = new LinkedList<Track>();
     try {
-      Collection<Resource<DriveDto>> driveDTOs = driveClient.findByOwnerOid(user.getOid()).execute().body().getContent();
-      for(Resource<DriveDto> driveDtoResource: driveDTOs){
-        tracks.add( TrackAssembler.toBean(driveDtoResource.getContent()));
+      Collection<Resource<DriveDto>> driveDtos = driveClient.findByOwnerOid(user.getOid())
+          .execute().body().getContent();
+      for (Resource<DriveDto> driveDtoResource : driveDtos) {
+        tracks.add(TrackAssembler.toBean(driveDtoResource.getContent()));
       }
-    } catch (IOException e) {
-      e.printStackTrace();
+    } catch (IOException exception) {
+      exception.printStackTrace();
     }
     return tracks;
   }
@@ -103,12 +114,13 @@ public class BasicDataConnector implements DataConnector {
   public List<Route> getRoutesByUser(User user) {
     List<Route> routes = new LinkedList<Route>();
     try {
-      Collection<Resource<RouteDto>> routeDTOs = routeClient.findByOwnerOid(user.getOid()).execute().body().getContent();
-      for(Resource<RouteDto> routeDtoResource: routeDTOs){
-        routes.add( RouteAssembler.toBean(routeDtoResource.getContent()));
+      Collection<Resource<RouteDto>> routeDtos = routeClient.findByOwnerOid(user.getOid())
+          .execute().body().getContent();
+      for (Resource<RouteDto> routeDtoResource : routeDtos) {
+        routes.add(RouteAssembler.toBean(routeDtoResource.getContent()));
       }
-    } catch (IOException e) {
-      e.printStackTrace();
+    } catch (IOException exception) {
+      exception.printStackTrace();
     }
     return routes;
   }
@@ -117,8 +129,8 @@ public class BasicDataConnector implements DataConnector {
   public void addTrack(Track track) {
     try {
       driveClient.create(TrackAssembler.toDto(track)).execute();
-    } catch (IOException e) {
-      e.printStackTrace();
+    } catch (IOException exception) {
+      exception.printStackTrace();
     }
   }
 
@@ -126,8 +138,8 @@ public class BasicDataConnector implements DataConnector {
   public void deleteTrack(Track track) {
     try {
       driveClient.delete(track.getOid()).execute();
-    } catch (IOException e) {
-      e.printStackTrace();
+    } catch (IOException exception) {
+      exception.printStackTrace();
     }
   }
 
@@ -135,8 +147,8 @@ public class BasicDataConnector implements DataConnector {
   public void addRoute(Route route) {
     try {
       routeClient.create(RouteAssembler.toDto(route)).execute();
-    } catch (IOException e) {
-      e.printStackTrace();
+    } catch (IOException exception) {
+      exception.printStackTrace();
     }
   }
 
@@ -144,8 +156,8 @@ public class BasicDataConnector implements DataConnector {
   public void deleteRoute(Route route) {
     try {
       routeClient.delete(route.getOid()).execute();
-    } catch (IOException e) {
-      e.printStackTrace();
+    } catch (IOException exception) {
+      exception.printStackTrace();
     }
   }
 
@@ -153,8 +165,8 @@ public class BasicDataConnector implements DataConnector {
   public void createUser(User user) {
     try {
       userClient.create(UserAssembler.toDto(user)).execute();
-    } catch (IOException e) {
-      e.printStackTrace();
+    } catch (IOException exception) {
+      exception.printStackTrace();
     }
   }
 
@@ -162,8 +174,8 @@ public class BasicDataConnector implements DataConnector {
   public void changeUserData(User user) {
     try {
       userClient.update(user.getOid(), UserAssembler.toDto(user)).execute();
-    } catch (IOException e) {
-      e.printStackTrace();
+    } catch (IOException exception) {
+      exception.printStackTrace();
     }
   }
 
@@ -171,8 +183,8 @@ public class BasicDataConnector implements DataConnector {
   public void addFriend(User user, User friend) {
     try {
       userClient.addFriend(user.getOid(), friend.getOid().toString()).execute();
-    } catch (IOException e) {
-      e.printStackTrace();
+    } catch (IOException exception) {
+      exception.printStackTrace();
     }
   }
 
@@ -180,12 +192,13 @@ public class BasicDataConnector implements DataConnector {
   public List<User> getFriends(User user) {
     List<User> friends = new LinkedList<User>();
     try {
-      Collection<Resource<UserDto>> resources = userClient.getFriends(user.getOid()).execute().body().getContent();
-      for(Resource<UserDto> resource: resources){
+      Collection<Resource<UserDto>> resources = userClient.getFriends(user.getOid()).execute()
+          .body().getContent();
+      for (Resource<UserDto> resource : resources) {
         friends.add(UserAssembler.toBean(resource.getContent()));
       }
-    } catch (IOException e) {
-      e.printStackTrace();
+    } catch (IOException exception) {
+      exception.printStackTrace();
     }
     return friends;
   }
