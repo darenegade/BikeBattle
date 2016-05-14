6796b19d16cc3d1a10180ededc3dc1ea6e40b811
Nils Bernhardt <nils.bernhardt@live.de>
2016-05-14 15:57:14 +0200

    Implemented data connector which executes code in background and then calls a cosumer.

--- app/src/main/java/edu/hm/cs/bikebattle/app/data/BasicDataConnector.java
+++ app/src/main/java/edu/hm/cs/bikebattle/app/data/BasicDataConnector.java
@@ -2,15 +2,16 @@ package edu.hm.cs.bikebattle.app.data;
 
 import android.app.Activity;
 import android.location.Location;
-import android.util.Log;
 
 import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.Resources;
 
 import java.io.IOException;
 import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
 
+import edu.hm.cs.bikebattle.app.api.domain.BaseDto;
 import edu.hm.cs.bikebattle.app.api.domain.DriveDto;
 import edu.hm.cs.bikebattle.app.api.domain.RouteDto;
 import edu.hm.cs.bikebattle.app.api.domain.UserDto;
@@ -48,13 +49,19 @@ public class BasicDataConnector implements DataConnector {
    * Backend drive/track client.
    */
   private final DriveClient driveClient;
-
-  private final String address = "https://moan.cs.hm.edu:8443/BikeBattleBackend/users/";
-
+  /**
+   * Adress of the server. Remove.
+   */
+  private final String address = "https://moan.cs.hm.edu:8443/BikeBattleBackend/users/"; //TODO remove
+  /**
+   * Activity to run consumer methods on.
+   */
   private final Activity activity;
 
   /**
    * Creates the clients for the backend.
+   *
+   * @param activity Activity to run consumer tasks on.
    */
   public BasicDataConnector(Activity activity) {
     this.activity = activity;
@@ -63,201 +70,220 @@ public class BasicDataConnector implements DataConnector {
     driveClient = ClientFactory.getDriveClient();
   }
 
-  @Override
-  public void getRoutesByLocation(final Location location, final float distance, final Consumer<List<Route>> consumer) {
+  /**
+   * Checks the Http code. If the Code is not successful the consumer is notified.
+   *
+   * @param code     http code
+   * @param consumer to notify
+   * @return true if successful
+   */
+  private boolean checkHttpCode(int code, Consumer consumer) {
+    if (code / 100 == 2) {
+      return true;
+    } else {
+      runErrorOnUiThread(consumer, code, null);
+      return false;
+    }
+  }
+
+  /**
+   * Runs the consumer consume method on the UI thread of the activity.
+   *
+   * @param consumer consumer
+   * @param argument argument for consume
+   * @param <T>      Consumer value
+   */
+  private <T> void runConsumerOnUiThread(final Consumer<T> consumer, final T argument) {
+    activity.runOnUiThread(new Runnable() {
+      @Override
+      public void run() {
+        consumer.consume(argument);
+      }
+    });
+  }
+
+  /**
+   * Runs the error method of the consumer on the activity UI thread.
+   *
+   * @param consumer  consumer
+   * @param code      error code
+   * @param exception thrown exception
+   */
+  private void runErrorOnUiThread(final Consumer consumer, final int code,
+                                  final IOException exception) {
+    activity.runOnUiThread(new Runnable() {
+      @Override
+      public void run() {
+        consumer.error(code, exception);
+      }
+    });
+  }
+
+  /**
+   * Converts the dto to bean.
+   *
+   * @param dto to convert.
+   * @return converted object
+   */
+  private Object toBean(BaseDto dto) {
+    if (dto.getClass().equals(RouteDto.class)) {
+      return RouteAssembler.toBean((RouteDto) dto);
+    } else if (dto.getClass().equals(DriveDto.class)) {
+      return TrackAssembler.toBean((DriveDto) dto);
+    } else if (dto.getClass().equals(UserDto.class)) {
+      return UserAssembler.toBean((UserDto) dto);
+    } else {
+      throw new IllegalArgumentException("Unknown dto type.");
+    }
+  }
+
+  /**
+   * Executes a get call for a List of resources.
+   *
+   * @param call     to execute
+   * @param consumer consumer for result
+   * @param <T>      dto
+   * @param <V>      bean
+   */
+  private <T extends BaseDto, V> void executeGetListCall(final Call<Resources<Resource<T>>> call,
+                                                         final Consumer<List<V>> consumer) {
     new Thread() {
       @Override
       public void run() {
-        List<Route> routes = new LinkedList<Route>();
         try {
-          Collection<Resource<RouteDto>> resources = routeClient.findNear(location.getLongitude(),
-              location.getLatitude(), distance).execute().body().getContent();
-          for (Resource<RouteDto> resource : resources) {
-            routes.add(RouteAssembler.toBean(resource.getContent()));
+          Response<Resources<Resource<T>>> response = call.execute();
+          if (checkHttpCode(response.code(), consumer)) {
+            List<V> list = new LinkedList<V>();
+            Collection<Resource<T>> resources = response.body().getContent();
+            for (Resource<T> resource : resources) {
+              list.add((V) toBean(resource.getContent()));
+            }
+            runConsumerOnUiThread(consumer, list);
           }
         } catch (IOException exception) {
-          exception.printStackTrace();
-          consumer.error(0);
-
+          runErrorOnUiThread(consumer, Consumer.IO_EXCEPTION, exception);
         }
-        consumer.consume(routes);
       }
     }.start();
+  }
 
+  /**
+   * Executes a get call for a resource.
+   *
+   * @param call     to execute
+   * @param consumer consumer for result
+   * @param <T>      dto
+   * @param <V>      bean
+   */
+  private <T extends BaseDto, V> void executeGetCall(final Call<Resource<T>> call,
+                                                     final Consumer<V> consumer) {
+    new Thread() {
+      @Override
+      public void run() {
+        try {
+          Response<Resource<T>> response = call.execute();
+          if (checkHttpCode(response.code(), consumer)) {
+            runConsumerOnUiThread(consumer, (V) toBean(response.body().getContent()));
+          }
+        } catch (IOException exception) {
+          runErrorOnUiThread(consumer, Consumer.IO_EXCEPTION, exception);
+        }
+      }
+    }.start();
+  }
+
+  /**
+   * Executes a write call.
+   *
+   * @param call     to execute
+   * @param consumer for errors
+   */
+  private void executeWriteCall(final Call<Void> call, final Consumer consumer) {
+    new Thread() {
+      @Override
+      public void run() {
+        try {
+          checkHttpCode(call.execute().code(), consumer);
+        } catch (IOException exception) {
+          runErrorOnUiThread(consumer, Consumer.IO_EXCEPTION, exception);
+        }
+      }
+    }.start();
+  }
+
+  @Override
+  public void getRoutesByLocation(final Location location, final float distance,
+                                  final Consumer<List<Route>> consumer) {
+    executeGetListCall(routeClient.findNear(location.getLongitude(),
+        location.getLatitude(), distance), consumer);
   }
 
   @Override
   public void getUserById(final String id, final Consumer<User> consumer) {
-    new Thread() {
-      @Override
-      public void run() {
-        Call<Resource<UserDto>> call = userClient.findeOne(id);
-        try {
-          //TODO check response code (see test cases) and handle errors
-          Response<Resource<UserDto>> userDtoResponse = call.execute();
-          Resource<UserDto> resource = userDtoResponse.body();
-          UserDto userDto = resource.getContent();
-          User user = UserAssembler.toBean(userDto);
-          consumer.consume(user);
-        } catch (IOException exception) {
-          exception.printStackTrace();
-        }
-      }
-    }.start();
+    executeGetCall(userClient.findeOne(id), consumer);
   }
 
   @Override
-  public List<User> getUserByName(String name) {
-    List<User> users = new LinkedList<User>();
-    try {
-      //TODO check response code (see test cases) and handle errors
-      Collection<Resource<UserDto>> userDtos = userClient.findByNameContainingIgnoreCase(name)
-          .execute().body().getContent();
-      for (Resource<UserDto> userDtoResource : userDtos) {
-        users.add(UserAssembler.toBean(userDtoResource.getContent()));
-      }
-    } catch (IOException exception) {
-      exception.printStackTrace();
-    }
-    return users;
+  public void getUserByName(String name, Consumer<List<User>> consumer) {
+    executeGetListCall(userClient.findByNameContainingIgnoreCase(name), consumer);
   }
 
   @Override
-  public List<Track> getTracksByUser(User user) {
-    List<Track> tracks = new LinkedList<Track>();
-    try {
-      Collection<Resource<DriveDto>> driveDtos = driveClient.findByOwnerOid(user.getOid())
-          .execute().body().getContent();
-      for (Resource<DriveDto> driveDtoResource : driveDtos) {
-        tracks.add(TrackAssembler.toBean(driveDtoResource.getContent()));
-      }
-    } catch (IOException exception) {
-      exception.printStackTrace();
-    }
-    return tracks;
+  public void getTracksByUser(User user, Consumer<List<Track>> consumer) {
+    executeGetListCall(driveClient.findByOwnerOid(user.getOid()), consumer);
   }
 
   @Override
-  public List<Route> getRoutesByUser(User user) {
-    List<Route> routes = new LinkedList<Route>();
-    try {
-      Collection<Resource<RouteDto>> routeDtos = routeClient.findByOwnerOid(user.getOid())
-          .execute().body().getContent();
-      for (Resource<RouteDto> routeDtoResource : routeDtos) {
-        routes.add(RouteAssembler.toBean(routeDtoResource.getContent()));
-      }
-    } catch (IOException exception) {
-      exception.printStackTrace();
-    }
-    return routes;
+  public void getRoutesByUser(User user, Consumer<List<Route>> consumer) {
+    executeGetListCall(routeClient.findByOwnerOid(user.getOid()), consumer);
   }
 
   @Override
-  public void addTrack(Track track, User owner) {
-    try {
-      DriveDto driveDto = TrackAssembler.toDto(track);
-      driveDto.setOwner(address + owner.getOid().toString());
-      driveClient.create(driveDto).execute();
-    } catch (IOException exception) {
-      exception.printStackTrace();
-    }
+  public void addTrack(Track track, User owner, Consumer consumer) {
+    DriveDto driveDto = TrackAssembler.toDto(track);
+    driveDto.setOwner(address + owner.getOid().toString()); //TODO rene backend
+    executeWriteCall(driveClient.create(driveDto), consumer);
   }
 
   @Override
-  public void deleteTrack(Track track) {
-    try {
-      driveClient.delete(track.getOid()).execute();
-    } catch (IOException exception) {
-      exception.printStackTrace();
-    }
+  public void deleteTrack(Track track, Consumer consumer) {
+    executeWriteCall(driveClient.delete(track.getOid()), consumer);
   }
 
   @Override
-  public void addRoute(Route route, User user) {
-    try {
-      RouteDto routeDto = RouteAssembler.toDto(route);
-      String owner = address + user.getOid();
-      Log.d("owner", owner);
-      routeDto.setOwner(owner);
-      routeClient.create(routeDto).execute();
-    } catch (IOException exception) {
-      exception.printStackTrace();
-    }
+  public void addRoute(Route route, User user, Consumer consumer) {
+    RouteDto routeDto = RouteAssembler.toDto(route);
+    routeDto.setOwner(address + user.getOid()); //TODO rene backend
+    executeWriteCall(routeClient.create(routeDto), consumer);
   }
 
   @Override
-  public void deleteRoute(Route route) {
-    try {
-      routeClient.delete(route.getOid()).execute();
-    } catch (IOException exception) {
-      exception.printStackTrace();
-    }
+  public void deleteRoute(Route route, Consumer consumer) {
+    executeWriteCall(routeClient.delete(route.getOid()), consumer);
   }
 
   @Override
-  public void createUser(User user) {
-    try {
-      userClient.create(UserAssembler.toDto(user)).execute();
-    } catch (IOException exception) {
-      exception.printStackTrace();
-    }
+  public void createUser(User user, Consumer consumer) {
+    executeWriteCall(userClient.create(UserAssembler.toDto(user)), consumer);
   }
 
   @Override
-  public void changeUserData(User user) {
-    try {
-      userClient.update(user.getOid(), UserAssembler.toDto(user)).execute();
-    } catch (IOException exception) {
-      exception.printStackTrace();
-    }
+  public void changeUserData(User user, Consumer consumer) {
+    executeWriteCall(userClient.update(user.getOid(), UserAssembler.toDto(user)), consumer);
   }
 
   @Override
-  public void addFriend(User user, User friend) {
-    try {
-      userClient.addFriend(user.getOid(), friend.getOid().toString()).execute();
-    } catch (IOException exception) {
-      exception.printStackTrace();
-    }
+  public void addFriend(User user, User friend, Consumer consumer) {
+    executeWriteCall(userClient.addFriend(user.getOid(), friend.getOid().toString()), consumer);
   }
 
   @Override
-  public List<User> getFriends(User user) {
-    List<User> friends = new LinkedList<User>();
-    try {
-      Collection<Resource<UserDto>> resources = userClient.getFriends(user.getOid()).execute()
-          .body().getContent();
-      for (Resource<UserDto> resource : resources) {
-        friends.add(UserAssembler.toBean(resource.getContent()));
-      }
-    } catch (IOException exception) {
-      exception.printStackTrace();
-    }
-    return friends;
+  public void getFriends(User user, Consumer<List<User>> consumer) {
+    executeGetListCall(userClient.getFriends(user.getOid()), consumer);
   }
 
   @Override
   public void getAllRoutes(final Consumer<List<Route>> consumer) {
-    new Thread() {
-      @Override
-      public void run() {
-        final List<Route> routes = new LinkedList<Route>();
-        try {
-          Collection<Resource<RouteDto>> resources = routeClient.findAll().execute().body().getContent();
-          for (Resource<RouteDto> resource : resources) {
-            routes.add(RouteAssembler.toBean(resource.getContent()));
-          }
-        } catch (IOException exception) {
-          exception.printStackTrace();
-          consumer.error(0);
-        }
-        activity.runOnUiThread(new Runnable() {
-          @Override
-          public void run() {
-            consumer.consume(routes);
-          }
-        });
-      }
-    }.start();
+    executeGetListCall(routeClient.findAll(), consumer);
   }
 }
--- app/src/main/java/edu/hm/cs/bikebattle/app/data/Consumer.java
+++ app/src/main/java/edu/hm/cs/bikebattle/app/data/Consumer.java
@@ -1,12 +1,33 @@
 package edu.hm.cs.bikebattle.app.data;
 
+import java.io.IOException;
+
 /**
  * Created by Nils on 10.05.2016.
+ * Interface for executing code after a specific tasks.
+ * Consume should be called with the result on success.
+ * Error should be called withe the reason on failure.
+ *
+ * @author Nils Bernhardt
+ * @version 1.0
  */
 public interface Consumer<T> {
+  /**
+   * Failure code if an exception was thrown.
+   */
+  public static final int IO_EXCEPTION = -1;
 
+  /**
+   * Method to execute on success.
+   * @param input result
+   */
   public void consume(T input);
 
-  public void error(int error);
+  /**
+   * Method to execute on failure.
+   * @param error code
+   * @param exception thrown exception (may be null)
+   */
+  public void error(int error, IOException exception);
 }
 
